b60315bb20a94bbf8bddac22a6d4984f556a62d0
tniu0003 <tniu0003@student.monahs.edu>
2024-08-02 22:19:27 +1000

    Activity 1: Import JSON files

--- tniu-library/src/App.vue
+++ tniu-library/src/App.vue
@@ -1,19 +1,21 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+//import TheWelcome from './components/TheWelcome.vue'
+import JSON from './components/JSON.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
+    
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
     </div>
   </header>
 
+  <JSON />
+
   <main>
-    <TheWelcome />
+    //<TheWelcome />
   </main>
 </template>
 
