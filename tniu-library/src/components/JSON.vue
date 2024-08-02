d5fa1ac3ffb60480a708cabf4a4cf6604bb17d3b
tniu0003 <tniu0003@student.monahs.edu>
2024-08-02 22:34:03 +1000

    Activity 5: Find author by ID

--- tniu-library/src/components/JSON.vue
+++ tniu-library/src/components/JSON.vue
@@ -113,9 +113,11 @@ const orwell = computed(() =>
 )
 
 // Activity 5: Find author by ID
-const austen = computed(() => {
+const austen = computed(() => 
   // TODO: CODE TO FIND AUTHOR BY ID HERE
-})
+  authors.find(author => author.id === 1))
+}
+
 </script>
 
 <style scoped>
